24af69834f182f30b8bf40c0f6df8fc65d0d6fcb
Shovit Nepal <nepalshovit@gmail.com>
2025-05-30 13:36:01 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// Toggle dropdown menu
+document.getElementById("menu-toggle").addEventListener("click", function () {
+   const dropdown = document.getElementById("dropdown");
+   dropdown.classList.toggle("show"); 
+
+});
+
+// hide dropdown when clicking a link
+document.querySelectorAll("#dropdown button").forEach((link) => {
+    link.addEventListener("click", () => {
+        document.getElementById("dropdown").classList.remove("show");
+
+    });
+});
+
+//click outside the box close the menu 
+document.addEventListener("click", (e) => {
+  const dropdown = document.getElementById("dropdown");
+  const toggle = document.getElementById("menu-toggle");
+
+  if (!dropdown.contains(e.target) && !toggle.contains(e.target)) {
+    dropdown.classList.remove("show");
+  }
+});
+
+//onclick teh menu button 
+function toScreen(id){
+  const element = document.getElementById(id);
+  if(element){
+  element.scrollIntoView({behavior: "smooth"});
+}
+else {
+console.error("Element with ID '" + id + "' not found.");
+}};
+
+// copyright script
+ 
+ const dates = new Date();
+ const fullYear = dates.getFullYear();
+ document.getElementById("date").innerHTML =`© ${fullYear} All Rights reserved.`;
+ 
+ document.addEventListener("DOMContentLoaded", () => {
+   const fills = document.querySelectorAll(".skill-fill");
+
+   const observer = new IntersectionObserver(
+     (entries) => {
+       entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+           const fill = entry.target;
+           const percentage = fill.textContent.trim();
+           fill.style.width = percentage;
+         }
+       });
+     },
+     {
+       threshold: 0.5,
+     }
+   );
+
+   fills.forEach((fill) => observer.observe(fill));
+ });
+
+
